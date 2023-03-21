ee815c7aef5293e6ddc22a74faa9bb56a0e73e73
Vojtěch Trunda <vojta478@gmail.com>
2023-03-21 20:35:40 +0100

    DB settings in progress

---
+++ eshopBackend.DAL/DbSettings/DbMapper.cs
@@ -0,0 +1,16 @@
+﻿using eshopBackend.DAL.Entities;
+using LinqToDB;
+
+namespace eshopBackend.DAL.DbSettings;
+
+public class DbMapper : LinqToDB.Data.DataConnection
+{
+
+        public DbMapper() : base("EshopBackendDb") { }
+
+        public ITable<EntityProduct>  Product  => this.GetTable<EntityProduct>();
+        public ITable<EntityCategory> Category => this.GetTable<EntityCategory>();
+
+
+
+}
