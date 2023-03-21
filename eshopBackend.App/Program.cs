b4f09df0ca77caa108c3d6716004be2d39dacad4
Vojtěch Trunda <vojta478@gmail.com>
2023-03-21 20:36:44 +0100

    App test

--- eshopBackend.App/Program.cs
+++ eshopBackend.App/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace eshopBackend.App;
+﻿using eshopBackend.DAL.DbSettings;
+using eshopBackend.DAL.Entities;
+using LinqToDB;
+using LinqToDB.Data;
+
+namespace eshopBackend.App;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        DataConnection.DefaultSettings = new DbSettings();
+
         Console.WriteLine("Hello, World!");
+
+
+        using (var db = new EshopBackendDb("eshopBackendDB"))
+            //using (var db = new DbMapper())
+        {
+            /*var q =
+                from c in db.Products
+                select c;
+
+            foreach (var c in q)
+                Console.WriteLine(c.ContactName);*/
+
+            try
+            {
+                db.CreateTable<EntityProduct>();
+                db.CreateTable<EntityCategory>();
+                db.CreateTable<EntityRating>();
+                db.CreateTable<EntityVendor>();
+            }
+            catch(MySqlConnector.MySqlException){}
+
+            
+        }
     }
 }
