e520be5366fb68109fed23e2e051b6eb20542bdf
Vojtěch Trunda <vojta478@gmail.com>
2023-03-21 20:34:30 +0100

    Undone entity mapper and repo interface

---
+++ eshopBackend.DAL/Mappers/IEntityMapper.cs
@@ -0,0 +1,9 @@
+﻿using eshopBackend.DAL.Entities;
+
+namespace eshopBackend.DAL.Mappers;
+
+public interface IEntityMapper<in TEntity>
+    where TEntity : IEntity
+{
+    void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
+}
