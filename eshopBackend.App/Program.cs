30e24e25f1a70175f964b6bac794978a28db3be0
Vojtěch Trunda <vojta478@gmail.com>
2023-03-23 21:21:38 +0100

    Made test app simpler

--- eshopBackend.App/Program.cs
+++ eshopBackend.App/Program.cs
@@ -1,7 +1,5 @@
-﻿using eshopBackend.DAL.DbSettings;
-using eshopBackend.DAL.Entities;
-using LinqToDB;
-using LinqToDB.Data;
+﻿using eshopBackend.DAL;
+using eshopBackend.DAL.DbSettings;
 
 namespace eshopBackend.App;
 
@@ -9,31 +7,10 @@ class Program
 {
     public static void Main(string[] args)
     {
-        DataConnection.DefaultSettings = new DbSettings();
+        DataAccessLayer dataAccessLayer = new();
 
+        //dataAccessLayer.Command<DbReset>();
+        
         Console.WriteLine("Hello, World!");
-
-
-        using (var db = new EshopBackendDb("eshopBackendDB"))
-            //using (var db = new DbMapper())
-        {
-            /*var q =
-                from c in db.Products
-                select c;
-
-            foreach (var c in q)
-                Console.WriteLine(c.ContactName);*/
-
-            try
-            {
-                db.CreateTable<EntityProduct>();
-                db.CreateTable<EntityCategory>();
-                db.CreateTable<EntityRating>();
-                db.CreateTable<EntityVendor>();
-            }
-            catch(MySqlConnector.MySqlException){}
-
-            
-        }
     }
 }
