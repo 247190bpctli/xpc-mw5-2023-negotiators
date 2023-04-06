9c56a27061c4bc0330cbc5b20ca5e7d820c474d0
Vojtěch Trunda <vojta478@gmail.com>
2023-04-06 11:14:56 +0200

    Removed unused Mapper and Repository interface, might be readded in the future

--- eshopBackend.DAL/Mappers/IEntityMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using eshopBackend.DAL.Entities;
-
-namespace eshopBackend.DAL.Mappers;
-
-public interface IEntityMapper<in TEntity>
-    where TEntity : IEntity
-{
-    void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
-}
