d8cbeab01f3ec23457a8dfa07f807b7a91578627
Filip <246976@vutbr.cz>
2023-04-11 21:59:28 +0200

    update Controllers

---
+++ eshopBackend.API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using eshopBackend.DAL;
+using eshopBackend.DAL.Entities;
+using eshopBackend.DAL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eshopBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+
+        //list products
+        [HttpGet("/list/{page}")]
+        public List<EntityProduct>? Get(byte page) => DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductsOverview(page);
+
+
+        //product detail
+        [HttpGet ("/detail/{id}")]
+        public EntityProduct? Get(string id) => DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductDetails(Guid.Parse(id));
+
+        //product add
+        [HttpPost("/add/{name},{imageUrl},{description},{price},{weight},{stock},{categoryId},{manufacturerId}")]
+        public Guid? Post(string name, string? imageUrl, string? description, double price, double weight, int stock, Guid? categoryId, Guid? manufacturerId)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductAdd(name, imageUrl, description, price, weight, stock, categoryId, manufacturerId);
+        }
+
+        //// PUT api/<ProductsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ProductsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
