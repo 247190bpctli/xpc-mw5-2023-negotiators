e0922a868d2d3dfc0241ed093cbc16a654af72c3
Vojtěch Trunda <vojta478@gmail.com>
2023-04-11 22:49:10 +0200

    Testing JSON

--- eshopBackend.App/Program.cs
+++ eshopBackend.App/Program.cs
@@ -4,6 +4,7 @@ using eshopBackend.DAL.Entities;
 using eshopBackend.DAL.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 using LoggerFactory = eshopBackend.DAL.Factories.LoggerFactory;
 
 namespace eshopBackend.App;
@@ -20,14 +21,18 @@ class Program
         //DataAccessLayer.serviceProvider.GetRequiredService<MockDataGenerator>().MakeMockData(3);
         //DataAccessLayer.serviceProvider.GetRequiredService<CreateRecordTest>().ViewTest();
         
+        JsonSerializerOptions opts = new()
+        {
+            WriteIndented = true //pretty styling
+        };
+
         //List<EntityProduct> pr = DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductsOverview(1);
         EntityProduct pr = DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductDetails(Guid.Parse("396820f9-3259-4906-a06c-a4654bf6bf59"));
-        Console.WriteLine(JsonSerializer.Serialize(pr));
-        //todo entities to guids
         
+        Console.WriteLine(JsonSerializer.Serialize(pr, opts));
+
         //todo test services
-        
-        
+
         //config to log
         //serviceProvider.GetRequiredService<ConfigFactory>().LogConfigDebugView();
         
