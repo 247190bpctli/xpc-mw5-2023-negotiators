1d05ee1e2ad980c3cc795da352c4ffa46ba6b365
Filip <246976@vutbr.cz>
2023-04-12 10:24:53 +0200

    product controller

--- eshopBackend.API/Controllers/ProductsController.cs
+++ eshopBackend.API/Controllers/ProductsController.cs
@@ -2,9 +2,7 @@
 using eshopBackend.DAL.Entities;
 using eshopBackend.DAL.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace eshopBackend.API.Controllers
 {
@@ -16,9 +14,8 @@ namespace eshopBackend.API.Controllers
 
         private readonly ILogger<ProductsController> _logger;
         public ProductsController(ILogger<ProductsController> logger) => _logger = logger;
-       
 
-        //list products
+
         [HttpGet("/list/{page}")]
         public List<EntityProduct>? Get(byte page)
         {
@@ -26,8 +23,6 @@ namespace eshopBackend.API.Controllers
             return products;// new List<EntityProduct>();
         }
 
-
-        //product detail
         [HttpGet("/detail/{id}")]
         public EntityProduct? Get(Guid id)
         {
@@ -40,28 +35,33 @@ namespace eshopBackend.API.Controllers
             {
                 _logger.LogError("Product cannot be found: {ExceptionMsg}", ex.Message);
                 _logger.LogDebug("Stack trace: {StackTrace}", ex.StackTrace);
-                
+
                 return null;
             }
         }
 
-        //product add
         [HttpPost("/add/{name},{imageUrl},{description},{price},{weight},{stock},{categoryId},{manufacturerId}")]
         public Guid? Post(string name, string? imageUrl, string? description, double price, double weight, int stock, Guid? categoryId, Guid? manufacturerId)
         {
             return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductAdd(name, imageUrl, description, price, weight, stock, categoryId, manufacturerId);
         }
 
-        //// PUT api/<ProductsController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
+        [HttpPut("/edit/{id},{name},{imageUrl},{description},{price},{weight},{stock},{categoryId},{manufacturerId}")]
+        public bool Put(Guid id, string? name, string? imageUrl, string? description, double? price, double? weight, int? stock, Guid? categoryId, Guid? manufacturerId)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductEdit(id, name, imageUrl, description, price, weight, stock, categoryId, manufacturerId);
+        }
 
-        //// DELETE api/<ProductsController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+        [HttpDelete("/delete/{id}")]
+        public bool Delete(Guid id)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ProductDelete(id);
+        }
+
+        [HttpPost("/Review/{productId},{stars},{user},{description})")]
+        public bool Post(Guid productId, byte stars, string user, string? description)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ReviewAdd(productId, stars, user, description);
+        }
     }
 }
