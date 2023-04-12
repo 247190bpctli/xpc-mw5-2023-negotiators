0e65aa4e1d5d9351e4c74c9c87513536a9bff8a7
Filip <246976@vutbr.cz>
2023-04-12 12:40:44 +0200

    Cart Controller added

---
+++ eshopBackend.API/Controllers/CartController.cs
@@ -0,0 +1,76 @@
+﻿using eshopBackend.DAL.Entities;
+using eshopBackend.DAL.Services;
+using eshopBackend.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace eshopBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ILogger<CartController> _logger;
+        public CartController(ILogger<CartController> logger) => _logger = logger;
+
+
+        [HttpGet("detail/{id}")]
+        public EntityCart? Get(Guid id)
+        {
+            try
+            {
+                EntityCart? details = DataAccessLayer.serviceProvider?.GetService<Cart>()?.CartDetails(id);
+                return details;
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError("Product cannot be found: {ExceptionMsg}", ex.Message);
+                _logger.LogDebug("Stack trace: {StackTrace}", ex.StackTrace);
+
+                return null;
+            }
+        }
+
+        [HttpGet("Create/")]
+        public Guid? Create()
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Cart>().CartAdd();
+        }
+
+        [HttpPut("edit/{cartId},{deliveryType},{deliveryAddress},{paymentType},{paymentDetails}")]
+        public bool Put(Guid cartId, int? deliveryType, string? deliveryAddress, int? paymentType, string? paymentDetails)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Cart>().CartEdit(cartId, deliveryType, deliveryAddress, paymentType, paymentDetails);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public bool Delete(Guid id)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Cart>().CartDelete(id);
+        }
+
+        [HttpPatch("AddToCart/{cartId},{productId},{amount}")]
+        public bool AddToCart(Guid cartId, Guid productId, int amount)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Cart>().AddToCart(cartId, productId, amount);
+        }
+
+        [HttpGet("finalizeOrder/{cartId}")]
+        public bool FinalizeOrder(Guid cartId)
+        {
+            return DataAccessLayer.serviceProvider.GetRequiredService<Cart>().FinalizeOrder(cartId);
+        }
+
+
+
+
+
+
+        ////[HttpPost("Review/{productId},{stars},{user},{description})")]
+        //public bool Post(Guid productId, byte stars, string user, string? description)
+        //{
+        //    return DataAccessLayer.serviceProvider.GetRequiredService<Products>().ReviewAdd(productId, stars, user, description);
+        //}
+    }
+}
