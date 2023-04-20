80ee935969260f8d62c771d94743190d51deeed0
Vojtěch Trunda <vojta478@gmail.com>
2023-04-20 22:33:23 +0200

    OnModelCreation seed preparation method

--- eshopBackend.DAL/AppDbContext.cs
+++ eshopBackend.DAL/AppDbContext.cs
@@ -24,4 +24,10 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //seeds here
+        
+    }
 }
