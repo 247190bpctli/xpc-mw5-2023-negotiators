7302c53824be2e654e6069872d17028533d2ec20
Filip <246976@vutbr.cz>
2023-04-21 22:56:11 +0200

    some controller fixes

--- eshopBackend.DAL/DTOs/ProductDTO.cs
+++ eshopBackend.DAL/DTOs/ProductDTO.cs
@@ -6,28 +6,6 @@ using System.Threading.Tasks;
 
 namespace eshopBackend.DAL.DTOs
 {
-    public class ProductOverviewDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string? ImageUrl { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
-    }
-
-    public class ProductDetailsDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? Description { get; set; }
-        public double Price { get; set; }
-        public double Weight { get; set; }
-        public int Stock { get; set; }
-        public Guid? CategoryId { get; set; }
-        public Guid? ManufacturerId { get; set; }
-    }
-
     public class ProductAddDto
     {
         public string Name { get; set; }
