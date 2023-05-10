08626f4e8234a796414ff29c6c1485a44d1e72e9
Vojtěch Trunda <vojta478@gmail.com>
2023-05-11 00:54:05 +0200

    Cart cleanup worker untested

---
+++ eshopBackend.DAL/Workers/CartCleanupWorker.cs
@@ -0,0 +1,56 @@
+using eshopBackend.DAL.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace eshopBackend.DAL.Workers;
+
+public class CartCleanupWorker : BackgroundService
+{
+    private readonly AppDbContext _db;
+    private readonly ILogger<CartCleanupWorker> _logger;
+    private readonly TimeSpan _interval;
+    private readonly int _maxage;
+
+    public CartCleanupWorker(AppDbContext db, ILogger<CartCleanupWorker> logger, IConfiguration config)
+    {
+        _db = db;
+        _logger = logger;
+        
+        try
+        {
+            _interval = TimeSpan.FromHours(config.GetSection("Cart").GetValue<int>("RemovalInterval"));
+            _maxage = config.GetSection("Cart").GetValue<int>("MaxAge");
+        }
+        catch (NullReferenceException ex)
+        {
+            _logger.LogWarning("Cart removal interval and/or max age unset");
+            _logger.LogWarning("{ExceptionMessage}", ex.Message);
+            _interval = TimeSpan.FromHours(1);
+            _maxage = 1;
+        }
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new(_interval);
+        while (!stoppingToken.IsCancellationRequested
+               && await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            List<CartEntity> oldCarts = _db.Carts
+                .Where(c => c.LastEdit < DateTime.UtcNow.AddHours(_maxage))
+                .ToList();
+
+            if (oldCarts.Count != 0)
+            {
+                foreach (CartEntity cart in oldCarts)
+                {
+                    _db.Carts.Remove(cart);
+                }
+
+                await _db.SaveChangesAsync(stoppingToken);
+                _logger.LogInformation("Removed {Amount} carts older than {Timeframe} hours", oldCarts.Count, _maxage);
+            }
+        }
+    }
+}
