50028185abbae135289c6267acc5e9ed9cd0a335
Vojtěch Trunda <vojta478@gmail.com>
2023-05-10 15:40:56 +0200

    Category integration tests untested

--- eshopBackend.IntegrationTests/ControllerTests/CategoriesControllerTests.cs
+++ eshopBackend.IntegrationTests/ControllerTests/CategoriesControllerTests.cs
@@ -22,7 +22,7 @@ public class CategoriesControllerTests : IntegrationTest
     {
         AddCategoryDto test = new() { Name = "catAname", ImageUrl = "imurl", Description = "desc" };
         
-        StringContent stringContent = new(JsonSerializer.Serialize(test), UnicodeEncoding.UTF8, "application/json");
+        StringContent stringContent = new(JsonSerializer.Serialize(test), Encoding.UTF8, "application/json");
         
         string testGuid = _client.PostAsync("/api/Categories/add", stringContent)
             .Result.Content.ReadAsStringAsync().Result.Replace("\"", "");
@@ -43,154 +43,108 @@ public class CategoriesControllerTests : IntegrationTest
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         
-        List<CategoryEntity> data = JsonSerializer.Deserialize<List<CategoryEntity>>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions);
+        List<CategoryEntity> data = JsonSerializer.Deserialize<List<CategoryEntity>>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions)!;
 
         Assert.NotEmpty(data);
         
         MockDataDispose(testGuid);
     }
 
-        [Fact]
-        public async Task GetById_IfExists_ReturnsDetails()
-        {
-            Guid testGuid = MockDataSetup();
+    [Fact]
+    public async Task GetById_IfExists_ReturnsDetails()
+    {
+        Guid testGuid = MockDataSetup();
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testGuid}");
+        HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testGuid}");
 
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            CategoryEntity data = JsonSerializer.Deserialize<CategoryEntity>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions);
+        CategoryEntity data = JsonSerializer.Deserialize<CategoryEntity>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions)!;
 
-            Assert.Equal("catAname", data.Name);
-            Assert.Equal("imurl", data.ImageUrl);
-            Assert.Equal("desc", data.Description);
+        Assert.Equal("catAname", data.Name);
+        Assert.Equal("imurl", data.ImageUrl);
+        Assert.Equal("desc", data.Description);
             
-            MockDataDispose(testGuid);
-        }
+        MockDataDispose(testGuid);
+    }
 
-        [Fact]
-        public async Task GetById_IfMissing_Returns404()
-        {
-            Guid testGuid = MockDataSetup();
+    [Fact]
+    public async Task GetById_IfMissing_Returns404()
+    {
+        Guid testGuid = MockDataSetup();
             
-            Guid testInvalidGuid = Guid.NewGuid();
+        Guid testInvalidGuid = Guid.NewGuid();
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
+        HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
 
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             
-            MockDataDispose(testGuid);
-        }
+        MockDataDispose(testGuid);
+    }
         
-        //not working below
-        [Fact]
-        public async Task EditById_IfExists_Updates()
+    [Fact]
+    public async Task EditById_IfExists_Updates()
+    {
+        Guid testGuid = MockDataSetup();
+
+        EditCategoryDto testEdit = new()
         {
-            Guid testGuid = MockDataSetup();
-            
-            Guid testInvalidGuid = Guid.NewGuid();
+            Id = testGuid,
+            Name = "catBname",
+            ImageUrl = "imurl",
+            Description = "desc3"
+        };
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
+        StringContent stringContent = new(JsonSerializer.Serialize(testEdit), Encoding.UTF8, "application/json");
 
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        HttpResponseMessage response = await _client.PutAsync($"/api/Categories/edit/{testGuid}", stringContent);
+
+        CategoryEntity data = JsonSerializer.Deserialize<CategoryEntity>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions)!;
             
-            MockDataDispose(testGuid);
-        }
+        Assert.Equal("catBname", data.Name);
+        Assert.Equal("imurl", data.ImageUrl);
+        Assert.Equal("desc3", data.Description);
+            
+        MockDataDispose(testGuid);
+    }
         
-        [Fact]
-        public async Task DeleteById_IfExists_GetsDeleted()
-        {
-            Guid testGuid = MockDataSetup();
+    [Fact]
+    public async Task DeleteById_IfExists_GetsDeleted()
+    {
+        Guid testGuid = MockDataSetup();
             
-            Guid testInvalidGuid = Guid.NewGuid();
+        MockDataDispose(testGuid);
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
+        HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testGuid}");
 
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-            
-            MockDataDispose(testGuid);
-        }
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
         
-        [Fact]
-        public async Task SearchByName_IfFound_ReturnsDetails()
-        {
-            Guid testGuid = MockDataSetup();
+    [Fact]
+    public async Task SearchByName_IfFound_ReturnsDetails()
+    {
+        Guid testGuid = MockDataSetup();
             
-            Guid testInvalidGuid = Guid.NewGuid();
+        HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/catAname");
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
+        CategoryEntity data = JsonSerializer.Deserialize<CategoryEntity>(await response.Content.ReadAsStringAsync(), _jsonSerializerOptions)!;
 
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal("catAname", data.Name);
+        Assert.Equal("imurl", data.ImageUrl);
+        Assert.Equal("desc", data.Description);
             
-            MockDataDispose(testGuid);
-        }
+        MockDataDispose(testGuid);
+    }
         
-        [Fact]
-        public async Task SearchByName_IfNotFound_Returns404()
-        {
-            Guid testGuid = MockDataSetup();
+    [Fact]
+    public async Task SearchByName_IfNotFound_Returns404()
+    {
+        Guid testGuid = MockDataSetup();
             
-            Guid testInvalidGuid = Guid.NewGuid();
+        HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/catBname");
 
-            HttpResponseMessage response = await _client.GetAsync($"/api/Categories/details/{testInvalidGuid}");
-
-            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             
-            MockDataDispose(testGuid);
-        }
-
-        /*[Fact]
-        public async Task GetSummary_Always_ReturnsSummary()
-        {
-            var mockReviews = new BookReview[]
-            {
-                new(){ Id = 1, Title="A", Rating = 1 },
-                new(){ Id = 2, Title="B", Rating = 2 },
-                new(){ Id = 3, Title="C", Rating = 5 },
-                new(){ Id = 4, Title="B", Rating = 4 }
-
-            }.AsQueryable();
-
-            _factory.ReviewRepositoryMock.Setup(r => r.AllReviews).Returns(mockReviews);
-
-            var response = await _client.GetAsync("/BookReviews/summary");
-
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-            var data = JsonConvert.DeserializeObject<IEnumerable<BookReview>>(await response.Content.ReadAsStringAsync());
-
-
-            Assert.Collection(data,
-                r =>
-                {
-                    Assert.Equal("A", r.Title);
-                    Assert.Equal(1, r.Rating);
-                },
-                r =>
-                {
-                    Assert.Equal("B", r.Title);
-                    Assert.Equal(3, r.Rating);
-                },
-                r =>
-                {
-                    Assert.Equal("C", r.Title);
-                    Assert.Equal(5, r.Rating);
-                }
-            );
-        }
-
-        [Fact]
-        public async Task Post_WithValidData_SavesReview()
-        {
-            var newReview = new BookReview { Title = "NewTitle", Rating = 4 };
-
-            _factory.ReviewRepositoryMock.Setup(r => r.Create(It.Is<BookReview>(b => b.Title == "NewTitle" && b.Rating == 4))).Verifiable();
-            _factory.ReviewRepositoryMock.Setup(r => r.SaveChanges()).Verifiable();
-
-            var response = await _client.PostAsync("/BookReviews", JsonContent.Create(newReview));
-
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-
-            _factory.ReviewRepositoryMock.VerifyAll();
-        }*/
+        MockDataDispose(testGuid);
+    }
 }
