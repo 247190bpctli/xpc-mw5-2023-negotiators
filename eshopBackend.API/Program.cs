d77f433513e7cb37eed8b6169c29e9a6b4acf47f
Vojtěch Trunda <vojta478@gmail.com>
2023-05-10 18:53:53 +0200

    Health checks for Docker

--- eshopBackend.API/Program.cs
+++ eshopBackend.API/Program.cs
@@ -17,13 +17,18 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
+        
+        //health checks (primarily for Docker)
+        builder.Services.AddHealthChecks();
+        
         //use user secrets if any
         builder.Configuration.AddUserSecrets<Program>(true);
 
         builder.Services.RegisterDalDependencies();
 
         var app = builder.Build();
+
+        app.MapHealthChecks("/health");
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
